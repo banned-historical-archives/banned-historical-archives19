4ddd25388e3767f36e056cd3460dffeb661c0637
Your Name <you@example.com>
2024-08-07 23:05:01 +0800

    1

---
+++ a2156ec4-452c-4d22-9035-81bd25430805.ts
@@ -0,0 +1,33 @@
+export default {
+  "entity": {
+    "id": "a2156ec4-452c-4d22-9035-81bd25430805",
+    "name": "1957年的夏季从百家争鸣到两家争鸣",
+    "internal": false,
+    "type": "pdf",
+    "official": false,
+    "author": "朱正",
+    "files": "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives19/main/a2156ec4-452c-4d22-9035-81bd25430805.pdf"
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "1957年的夏季从百家争鸣到两家争鸣",
+        "authors": [
+          "朱正"
+        ],
+        "page_start": 1,
+        "page_end": 584,
+        "dates": [
+          {
+            "year": 1998,
+            "month": 5
+          }
+        ]
+      }
+    ]
+  },
+  "parser_id": "automation",
+  "path": "a2156ec4-452c-4d22-9035-81bd25430805.pdf",
+  "resource_type": "book",
+  "version": 2
+}
