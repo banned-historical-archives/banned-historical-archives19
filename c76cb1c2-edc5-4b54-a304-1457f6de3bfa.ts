fe294574fc1ea546812bc5f7dab48a78fc2287c4
Your Name <you@example.com>
2024-08-07 21:39:19 +0800

    1

---
+++ c76cb1c2-edc5-4b54-a304-1457f6de3bfa.ts
@@ -0,0 +1,32 @@
+export default {
+  "entity": {
+    "id": "c76cb1c2-edc5-4b54-a304-1457f6de3bfa",
+    "name": "广西文革大事年表",
+    "internal": false,
+    "type": "pdf",
+    "official": false,
+    "author": "广西文革大事年表编写小组",
+    "files": "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives19/main/c76cb1c2-edc5-4b54-a304-1457f6de3bfa.pdf"
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "广西文革大事年表",
+        "authors": [
+          "广西文革大事年表编写小组"
+        ],
+        "page_start": 1,
+        "page_end": 310,
+        "dates": [
+          {
+            "year": 1990
+          }
+        ]
+      }
+    ]
+  },
+  "parser_id": "automation",
+  "path": "c76cb1c2-edc5-4b54-a304-1457f6de3bfa.pdf",
+  "resource_type": "book",
+  "version": 2
+}
