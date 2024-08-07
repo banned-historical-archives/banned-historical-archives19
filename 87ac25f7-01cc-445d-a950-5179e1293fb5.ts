2f21df3542cbbb8dc53abed79ffc6a44618e6c93
Your Name <you@example.com>
2024-08-07 21:18:03 +0800

    1

---
+++ 87ac25f7-01cc-445d-a950-5179e1293fb5.ts
@@ -0,0 +1,33 @@
+export default {
+  "entity": {
+    "id": "87ac25f7-01cc-445d-a950-5179e1293fb5",
+    "name": "变局——七千人大会始末",
+    "internal": false,
+    "type": "pdf",
+    "official": false,
+    "author": "张素华",
+    "files": "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives19/main/87ac25f7-01cc-445d-a950-5179e1293fb5.pdf"
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "变局——七千人大会始末",
+        "authors": [
+          "张素华"
+        ],
+        "page_start": 1,
+        "page_end": 357,
+        "dates": [
+          {
+            "year": 2006,
+            "month": 6
+          }
+        ]
+      }
+    ]
+  },
+  "parser_id": "automation",
+  "path": "87ac25f7-01cc-445d-a950-5179e1293fb5.pdf",
+  "resource_type": "book",
+  "version": 2
+}
