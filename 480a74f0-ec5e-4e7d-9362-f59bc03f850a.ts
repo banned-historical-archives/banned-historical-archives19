dbe15b086f7e8dc48755026ed4676f50dc9c09ce
Your Name <you@example.com>
2024-08-07 23:03:55 +0800

    1

---
+++ 480a74f0-ec5e-4e7d-9362-f59bc03f850a.ts
@@ -0,0 +1,33 @@
+export default {
+  "entity": {
+    "id": "480a74f0-ec5e-4e7d-9362-f59bc03f850a",
+    "name": "在历史的漩涡中-中国百名大右派",
+    "internal": false,
+    "type": "pdf",
+    "official": false,
+    "author": "姚杉尔",
+    "files": "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives19/main/480a74f0-ec5e-4e7d-9362-f59bc03f850a.pdf"
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "在历史的漩涡中-中国百名大右派",
+        "authors": [
+          "姚杉尔"
+        ],
+        "page_start": 1,
+        "page_end": 546,
+        "dates": [
+          {
+            "year": 1993,
+            "month": 4
+          }
+        ]
+      }
+    ]
+  },
+  "parser_id": "automation",
+  "path": "480a74f0-ec5e-4e7d-9362-f59bc03f850a.pdf",
+  "resource_type": "book",
+  "version": 2
+}
