c8ccc84bd55fe0d9b65c7cbadd367927909118e0
Your Name <you@example.com>
2024-08-07 22:17:12 +0800

    1

---
+++ 1b478d75-8f55-4949-8238-4f0acf469c8e.ts
@@ -0,0 +1,32 @@
+export default {
+  "entity": {
+    "id": "1b478d75-8f55-4949-8238-4f0acf469c8e",
+    "name": "青春从文革战火走过",
+    "internal": false,
+    "type": "pdf",
+    "official": false,
+    "author": "李正权",
+    "files": "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives19/main/1b478d75-8f55-4949-8238-4f0acf469c8e.pdf"
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "青春从文革战火走过",
+        "authors": [
+          "李正权"
+        ],
+        "page_start": 1,
+        "page_end": 383,
+        "dates": [
+          {
+            "year": 2015
+          }
+        ]
+      }
+    ]
+  },
+  "parser_id": "automation",
+  "path": "1b478d75-8f55-4949-8238-4f0acf469c8e.pdf",
+  "resource_type": "book",
+  "version": 2
+}
