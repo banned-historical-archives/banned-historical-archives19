3c9fe1851d8a63d96e6259e2e79a94406edb9c64
Your Name <you@example.com>
2024-08-07 21:43:35 +0800

    1

---
+++ a377f260-a275-4cff-a8de-8bbd80d60ea1.ts
@@ -0,0 +1,26 @@
+export default {
+  "entity": {
+    "id": "a377f260-a275-4cff-a8de-8bbd80d60ea1",
+    "name": "山西省文化大革命资料选编",
+    "internal": false,
+    "type": "pdf",
+    "official": false,
+    "author": "",
+    "files": "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives19/main/a377f260-a275-4cff-a8de-8bbd80d60ea1.pdf"
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "山西省文化大革命资料选编",
+        "authors": [],
+        "page_start": 1,
+        "page_end": 1330,
+        "dates": []
+      }
+    ]
+  },
+  "parser_id": "automation",
+  "path": "a377f260-a275-4cff-a8de-8bbd80d60ea1.pdf",
+  "resource_type": "book",
+  "version": 2
+}
