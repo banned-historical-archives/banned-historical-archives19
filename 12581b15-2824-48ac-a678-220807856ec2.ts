1fbcc098ffa4ce227a7fcb618f33293c490dd1dd
Your Name <you@example.com>
2024-08-07 23:40:56 +0800

    1

---
+++ 12581b15-2824-48ac-a678-220807856ec2.ts
@@ -0,0 +1,28 @@
+export default {
+  "entity": {
+    "id": "12581b15-2824-48ac-a678-220807856ec2",
+    "name": "重审林彪罪案上册",
+    "internal": false,
+    "type": "pdf",
+    "official": false,
+    "author": "丁凯文",
+    "files": "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives19/main/12581b15-2824-48ac-a678-220807856ec2.pdf"
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "重审林彪罪案上册",
+        "authors": [
+          "丁凯文"
+        ],
+        "page_start": 1,
+        "page_end": 1012,
+        "dates": []
+      }
+    ]
+  },
+  "parser_id": "automation",
+  "path": "12581b15-2824-48ac-a678-220807856ec2.pdf",
+  "resource_type": "book",
+  "version": 2
+}
