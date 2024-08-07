57cf1e95fafe2ef4231ba4f190795e64ab55523e
Your Name <you@example.com>
2024-08-07 22:20:34 +0800

    1

---
+++ dc4bb1e6-490c-4045-9929-2b3af08610ae.ts
@@ -0,0 +1,33 @@
+export default {
+  "entity": {
+    "id": "dc4bb1e6-490c-4045-9929-2b3af08610ae",
+    "name": "云烟未散：记忆与反思",
+    "internal": false,
+    "type": "pdf",
+    "official": false,
+    "author": "宋书星",
+    "files": "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives19/main/dc4bb1e6-490c-4045-9929-2b3af08610ae.pdf"
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "云烟未散：记忆与反思",
+        "authors": [
+          "宋书星"
+        ],
+        "page_start": 1,
+        "page_end": 271,
+        "dates": [
+          {
+            "year": 2021,
+            "month": 3
+          }
+        ]
+      }
+    ]
+  },
+  "parser_id": "automation",
+  "path": "dc4bb1e6-490c-4045-9929-2b3af08610ae.pdf",
+  "resource_type": "book",
+  "version": 2
+}
