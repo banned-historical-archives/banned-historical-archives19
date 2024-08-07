f4b095823ccefdde4647e85ab8ad1b260bc4ac9b
Your Name <you@example.com>
2024-08-07 21:19:11 +0800

    1

---
+++ 1f8a7dbf-fe47-4ff2-a85f-1157d394a041.ts
@@ -0,0 +1,32 @@
+export default {
+  "entity": {
+    "id": "1f8a7dbf-fe47-4ff2-a85f-1157d394a041",
+    "name": "庐山会议实录",
+    "internal": false,
+    "type": "pdf",
+    "official": false,
+    "author": "李锐",
+    "files": "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives19/main/1f8a7dbf-fe47-4ff2-a85f-1157d394a041.pdf"
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "庐山会议实录",
+        "authors": [
+          "李锐"
+        ],
+        "page_start": 1,
+        "page_end": 319,
+        "dates": [
+          {
+            "year": 1999
+          }
+        ]
+      }
+    ]
+  },
+  "parser_id": "automation",
+  "path": "1f8a7dbf-fe47-4ff2-a85f-1157d394a041.pdf",
+  "resource_type": "book",
+  "version": 2
+}
