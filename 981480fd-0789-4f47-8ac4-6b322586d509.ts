fc10b289bed4d5422e81783016798145f6991233
Your Name <you@example.com>
2024-08-07 23:45:02 +0800

    1

---
+++ 981480fd-0789-4f47-8ac4-6b322586d509.ts
@@ -0,0 +1,28 @@
+export default {
+  "entity": {
+    "id": "981480fd-0789-4f47-8ac4-6b322586d509",
+    "name": "《人民文革论》（人民文革丛书卷四）",
+    "internal": false,
+    "type": "pdf",
+    "official": false,
+    "author": "宋永毅",
+    "files": "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives19/main/981480fd-0789-4f47-8ac4-6b322586d509.pdf"
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "《人民文革论》（人民文革丛书卷四）",
+        "authors": [
+          "宋永毅"
+        ],
+        "page_start": 1,
+        "page_end": 112,
+        "dates": []
+      }
+    ]
+  },
+  "parser_id": "automation",
+  "path": "981480fd-0789-4f47-8ac4-6b322586d509.pdf",
+  "resource_type": "book",
+  "version": 2
+}
