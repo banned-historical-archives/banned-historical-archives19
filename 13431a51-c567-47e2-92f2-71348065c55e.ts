fab13f436cc0a9615755102d5d72eec79db53489
Your Name <you@example.com>
2024-08-07 22:33:55 +0800

    1

---
+++ 13431a51-c567-47e2-92f2-71348065c55e.ts
@@ -0,0 +1,28 @@
+export default {
+  "entity": {
+    "id": "13431a51-c567-47e2-92f2-71348065c55e",
+    "name": "范正美回忆录——秋梦追思",
+    "internal": false,
+    "type": "pdf",
+    "official": false,
+    "author": "范正美",
+    "files": "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives19/main/13431a51-c567-47e2-92f2-71348065c55e.pdf"
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "范正美回忆录——秋梦追思",
+        "authors": [
+          "范正美"
+        ],
+        "page_start": 1,
+        "page_end": 441,
+        "dates": []
+      }
+    ]
+  },
+  "parser_id": "automation",
+  "path": "13431a51-c567-47e2-92f2-71348065c55e.pdf",
+  "resource_type": "book",
+  "version": 2
+}
