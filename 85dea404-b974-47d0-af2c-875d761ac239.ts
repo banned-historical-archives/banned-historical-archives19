42095629e86f77b6679ad0f7199b1d68cd9ff9ed
Your Name <you@example.com>
2024-08-07 23:06:47 +0800

    1

---
+++ 85dea404-b974-47d0-af2c-875d761ac239.ts
@@ -0,0 +1,26 @@
+export default {
+  "entity": {
+    "id": "85dea404-b974-47d0-af2c-875d761ac239",
+    "name": "高岗饶漱石事件始末",
+    "internal": false,
+    "type": "pdf",
+    "official": false,
+    "author": "",
+    "files": "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives19/main/85dea404-b974-47d0-af2c-875d761ac239.pdf"
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "高岗饶漱石事件始末",
+        "authors": [],
+        "page_start": 1,
+        "page_end": 282,
+        "dates": []
+      }
+    ]
+  },
+  "parser_id": "automation",
+  "path": "85dea404-b974-47d0-af2c-875d761ac239.pdf",
+  "resource_type": "book",
+  "version": 2
+}
