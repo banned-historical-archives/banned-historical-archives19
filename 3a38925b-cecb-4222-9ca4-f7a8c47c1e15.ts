ad06a6857488980466e4721a29d2104e1b925f07
Your Name <you@example.com>
2024-08-07 22:36:35 +0800

    1

---
+++ 3a38925b-cecb-4222-9ca4-f7a8c47c1e15.ts
@@ -0,0 +1,28 @@
+export default {
+  "entity": {
+    "id": "3a38925b-cecb-4222-9ca4-f7a8c47c1e15",
+    "name": "四十三年 望中犹记",
+    "internal": false,
+    "type": "pdf",
+    "official": false,
+    "author": "吴焱金",
+    "files": "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives19/main/3a38925b-cecb-4222-9ca4-f7a8c47c1e15.pdf"
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "四十三年 望中犹记",
+        "authors": [
+          "吴焱金"
+        ],
+        "page_start": 1,
+        "page_end": 123,
+        "dates": []
+      }
+    ]
+  },
+  "parser_id": "automation",
+  "path": "3a38925b-cecb-4222-9ca4-f7a8c47c1e15.pdf",
+  "resource_type": "book",
+  "version": 2
+}
