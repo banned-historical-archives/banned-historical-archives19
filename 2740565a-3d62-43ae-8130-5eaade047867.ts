5aad86bf462985ce9b49172e24a8d46d04e3ba52
Your Name <you@example.com>
2024-08-07 22:31:58 +0800

    1

---
+++ 2740565a-3d62-43ae-8130-5eaade047867.ts
@@ -0,0 +1,33 @@
+export default {
+  "entity": {
+    "id": "2740565a-3d62-43ae-8130-5eaade047867",
+    "name": "不再沉默——一个文革亲历者的回顾与思考",
+    "internal": false,
+    "type": "pdf",
+    "official": false,
+    "author": "武彩霞",
+    "files": "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives19/main/2740565a-3d62-43ae-8130-5eaade047867.pdf"
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "不再沉默——一个文革亲历者的回顾与思考",
+        "authors": [
+          "武彩霞"
+        ],
+        "page_start": 1,
+        "page_end": 384,
+        "dates": [
+          {
+            "year": 2010,
+            "month": 8
+          }
+        ]
+      }
+    ]
+  },
+  "parser_id": "automation",
+  "path": "2740565a-3d62-43ae-8130-5eaade047867.pdf",
+  "resource_type": "book",
+  "version": 2
+}
