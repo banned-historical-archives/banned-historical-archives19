14bdcfb32c6ad121f0e266aa3d91c31cabf1d7b8
Your Name <you@example.com>
2024-08-07 21:15:11 +0800

    1

---
+++ 6704e03b-6af9-4958-8fae-d4f31256a326.ts
@@ -0,0 +1,26 @@
+export default {
+  "entity": {
+    "id": "6704e03b-6af9-4958-8fae-d4f31256a326",
+    "name": "大跃进和人民公社运动：违背客观发展规律的一场空想共产主义运动",
+    "internal": false,
+    "type": "pdf",
+    "official": false,
+    "author": "",
+    "files": "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives19/main/6704e03b-6af9-4958-8fae-d4f31256a326.pdf"
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "大跃进和人民公社运动：违背客观发展规律的一场空想共产主义运动",
+        "authors": [],
+        "page_start": 1,
+        "page_end": 66,
+        "dates": []
+      }
+    ]
+  },
+  "parser_id": "automation",
+  "path": "6704e03b-6af9-4958-8fae-d4f31256a326.pdf",
+  "resource_type": "book",
+  "version": 2
+}
