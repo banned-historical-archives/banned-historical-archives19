cba43d78f748d3b493d19fa68eb27d14f59e0678
Your Name <you@example.com>
2024-08-07 22:22:36 +0800

    1

---
+++ 3bc42db2-1e41-4431-9658-3ebe7e6ca070.ts
@@ -0,0 +1,32 @@
+export default {
+  "entity": {
+    "id": "3bc42db2-1e41-4431-9658-3ebe7e6ca070",
+    "name": "回顾与展望——一棵“四人帮好苗子”的自述",
+    "internal": false,
+    "type": "pdf",
+    "official": false,
+    "author": "董学诚",
+    "files": "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives19/main/3bc42db2-1e41-4431-9658-3ebe7e6ca070.pdf"
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "回顾与展望——一棵“四人帮好苗子”的自述",
+        "authors": [
+          "董学诚"
+        ],
+        "page_start": 1,
+        "page_end": 240,
+        "dates": [
+          {
+            "year": 2016
+          }
+        ]
+      }
+    ]
+  },
+  "parser_id": "automation",
+  "path": "3bc42db2-1e41-4431-9658-3ebe7e6ca070.pdf",
+  "resource_type": "book",
+  "version": 2
+}
