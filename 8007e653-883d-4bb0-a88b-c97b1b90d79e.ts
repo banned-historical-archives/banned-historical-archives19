5913353c24c596c5d1838f0febd45b2d3a54402b
Your Name <you@example.com>
2024-08-07 22:47:19 +0800

    1

---
+++ 8007e653-883d-4bb0-a88b-c97b1b90d79e.ts
@@ -0,0 +1,33 @@
+export default {
+  "entity": {
+    "id": "8007e653-883d-4bb0-a88b-c97b1b90d79e",
+    "name": "风雨七十年",
+    "internal": false,
+    "type": "pdf",
+    "official": false,
+    "author": "张建华",
+    "files": "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives19/main/8007e653-883d-4bb0-a88b-c97b1b90d79e.pdf"
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "风雨七十年",
+        "authors": [
+          "张建华"
+        ],
+        "page_start": 1,
+        "page_end": 648,
+        "dates": [
+          {
+            "year": 2018,
+            "month": 4
+          }
+        ]
+      }
+    ]
+  },
+  "parser_id": "automation",
+  "path": "8007e653-883d-4bb0-a88b-c97b1b90d79e.pdf",
+  "resource_type": "book",
+  "version": 2
+}
