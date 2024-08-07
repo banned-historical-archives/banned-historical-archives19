681c210b17433154696c45fa1b3469c25c140c24
Your Name <you@example.com>
2024-08-08 00:01:58 +0800

    1

---
+++ 623fa8e3-e5ec-4fd3-8a31-2dd76fc45b7e.ts
@@ -0,0 +1,28 @@
+export default {
+  "entity": {
+    "id": "623fa8e3-e5ec-4fd3-8a31-2dd76fc45b7e",
+    "name": "失落的一代——中国的上山下乡运动1968-1980",
+    "internal": false,
+    "type": "pdf",
+    "official": false,
+    "author": "潘鸣啸",
+    "files": "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives19/main/623fa8e3-e5ec-4fd3-8a31-2dd76fc45b7e.pdf"
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "失落的一代——中国的上山下乡运动1968-1980",
+        "authors": [
+          "潘鸣啸"
+        ],
+        "page_start": 1,
+        "page_end": 466,
+        "dates": []
+      }
+    ]
+  },
+  "parser_id": "automation",
+  "path": "623fa8e3-e5ec-4fd3-8a31-2dd76fc45b7e.pdf",
+  "resource_type": "book",
+  "version": 2
+}
---
+++ e407021b-8c58-4a92-99dd-9da912a5863f.ts
@@ -0,0 +1,33 @@
+export default {
+  "entity": {
+    "id": "e407021b-8c58-4a92-99dd-9da912a5863f",
+    "name": "极端年代的公民政治——群众的文化大革命史",
+    "internal": false,
+    "type": "pdf",
+    "official": false,
+    "author": "童小溪",
+    "files": "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives19/main/e407021b-8c58-4a92-99dd-9da912a5863f.pdf"
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "极端年代的公民政治——群众的文化大革命史",
+        "authors": [
+          "童小溪"
+        ],
+        "page_start": 1,
+        "page_end": 450,
+        "dates": [
+          {
+            "year": 2011,
+            "month": 4
+          }
+        ]
+      }
+    ]
+  },
+  "parser_id": "automation",
+  "path": "e407021b-8c58-4a92-99dd-9da912a5863f.pdf",
+  "resource_type": "book",
+  "version": 2
+}
