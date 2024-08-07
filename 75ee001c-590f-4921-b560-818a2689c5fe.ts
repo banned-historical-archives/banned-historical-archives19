7ed178cb4cd4717190df971e6868ab59d9821478
Your Name <you@example.com>
2024-08-07 22:09:22 +0800

    1

---
+++ 75ee001c-590f-4921-b560-818a2689c5fe.ts
@@ -0,0 +1,33 @@
+export default {
+  "entity": {
+    "id": "75ee001c-590f-4921-b560-818a2689c5fe",
+    "name": "写给历史的交代——原重庆八一五派「文攻武卫总司令」自述",
+    "internal": false,
+    "type": "pdf",
+    "official": false,
+    "author": "杨增泰",
+    "files": "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives19/main/75ee001c-590f-4921-b560-818a2689c5fe.pdf"
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "写给历史的交代——原重庆八一五派「文攻武卫总司令」自述",
+        "authors": [
+          "杨增泰"
+        ],
+        "page_start": 1,
+        "page_end": 395,
+        "dates": [
+          {
+            "year": 2014,
+            "month": 11
+          }
+        ]
+      }
+    ]
+  },
+  "parser_id": "automation",
+  "path": "75ee001c-590f-4921-b560-818a2689c5fe.pdf",
+  "resource_type": "book",
+  "version": 2
+}
