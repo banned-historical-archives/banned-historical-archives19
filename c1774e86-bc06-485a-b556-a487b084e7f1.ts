2772888b33805f77c0131adf47880a21c139cfaa
Your Name <you@example.com>
2024-08-07 22:50:45 +0800

    1

---
+++ c1774e86-bc06-485a-b556-a487b084e7f1.ts
@@ -0,0 +1,32 @@
+export default {
+  "entity": {
+    "id": "c1774e86-bc06-485a-b556-a487b084e7f1",
+    "name": "聂元梓回忆录（上）",
+    "internal": false,
+    "type": "pdf",
+    "official": false,
+    "author": "聂元梓",
+    "files": "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives19/main/c1774e86-bc06-485a-b556-a487b084e7f1.pdf"
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "聂元梓回忆录（上）",
+        "authors": [
+          "聂元梓"
+        ],
+        "page_start": 1,
+        "page_end": 266,
+        "dates": [
+          {
+            "year": 2005
+          }
+        ]
+      }
+    ]
+  },
+  "parser_id": "automation",
+  "path": "c1774e86-bc06-485a-b556-a487b084e7f1.pdf",
+  "resource_type": "book",
+  "version": 2
+}
