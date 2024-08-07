2843218b6537d1e98e9ee2063b5a339ad199e76e
Your Name <you@example.com>
2024-08-07 22:33:00 +0800

    1

---
+++ 328aeba2-613b-41bf-8ad3-a5239c23a7ef.ts
@@ -0,0 +1,34 @@
+export default {
+  "entity": {
+    "id": "328aeba2-613b-41bf-8ad3-a5239c23a7ef",
+    "name": "永远的造反派——袁庚华先生访谈录全文",
+    "internal": false,
+    "type": "pdf",
+    "official": false,
+    "author": "陈宜中 袁庚华",
+    "files": "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives19/main/328aeba2-613b-41bf-8ad3-a5239c23a7ef.pdf"
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "永远的造反派——袁庚华先生访谈录全文",
+        "authors": [
+          "陈宜中",
+          "袁庚华"
+        ],
+        "page_start": 1,
+        "page_end": 57,
+        "dates": [
+          {
+            "year": 2010,
+            "month": 7
+          }
+        ]
+      }
+    ]
+  },
+  "parser_id": "automation",
+  "path": "328aeba2-613b-41bf-8ad3-a5239c23a7ef.pdf",
+  "resource_type": "book",
+  "version": 2
+}
