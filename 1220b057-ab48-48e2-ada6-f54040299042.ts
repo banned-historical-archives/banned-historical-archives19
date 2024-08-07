792575041a21741e3513c530ebd55f73dd4a7a9a
Your Name <you@example.com>
2024-08-07 22:06:01 +0800

    1

---
+++ 1220b057-ab48-48e2-ada6-f54040299042.ts
@@ -0,0 +1,28 @@
+export default {
+  "entity": {
+    "id": "1220b057-ab48-48e2-ada6-f54040299042",
+    "name": "西藏造反派文革史——陶长松口述自",
+    "internal": false,
+    "type": "pdf",
+    "official": false,
+    "author": "陶长松",
+    "files": "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives19/main/1220b057-ab48-48e2-ada6-f54040299042.pdf"
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "西藏造反派文革史——陶长松口述自",
+        "authors": [
+          "陶长松"
+        ],
+        "page_start": 1,
+        "page_end": 22,
+        "dates": []
+      }
+    ]
+  },
+  "parser_id": "automation",
+  "path": "1220b057-ab48-48e2-ada6-f54040299042.pdf",
+  "resource_type": "book",
+  "version": 2
+}
