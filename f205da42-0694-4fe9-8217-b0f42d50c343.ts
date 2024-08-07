8a08ea99bf9ae8f9c71e4c20c376ec59b83df119
Your Name <you@example.com>
2024-08-07 22:51:50 +0800

    1

---
+++ f205da42-0694-4fe9-8217-b0f42d50c343.ts
@@ -0,0 +1,32 @@
+export default {
+  "entity": {
+    "id": "f205da42-0694-4fe9-8217-b0f42d50c343",
+    "name": "聂元梓回忆录（下）",
+    "internal": false,
+    "type": "pdf",
+    "official": false,
+    "author": "聂元梓",
+    "files": "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives19/main/f205da42-0694-4fe9-8217-b0f42d50c343.pdf"
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "聂元梓回忆录（下）",
+        "authors": [
+          "聂元梓"
+        ],
+        "page_start": 1,
+        "page_end": 269,
+        "dates": [
+          {
+            "year": 2005
+          }
+        ]
+      }
+    ]
+  },
+  "parser_id": "automation",
+  "path": "f205da42-0694-4fe9-8217-b0f42d50c343.pdf",
+  "resource_type": "book",
+  "version": 2
+}
