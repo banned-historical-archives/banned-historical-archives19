f71433254f24b5bf5cee8f151f90f70ef47d69e3
Your Name <you@example.com>
2024-08-07 21:04:06 +0800

    1

---
+++ 2a5a5a1a-7203-43f2-bf13-2881453e9777.ts
@@ -0,0 +1,31 @@
+export default {
+  "entity": {
+    "id": "2a5a5a1a-7203-43f2-bf13-2881453e9777",
+    "name": "本市延安中学在毛主席教育方针指引下打破“单打一”教学实行教师“多面手”",
+    "internal": false,
+    "type": "imgs",
+    "official": false,
+    "author": "",
+    "files": [
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives19/main/2a5a5a1a-7203-43f2-bf13-2881453e9777/1.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives19/main/2a5a5a1a-7203-43f2-bf13-2881453e9777/2.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives19/main/2a5a5a1a-7203-43f2-bf13-2881453e9777/3.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives19/main/2a5a5a1a-7203-43f2-bf13-2881453e9777/4.jpg"
+    ]
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "本市延安中学在毛主席教育方针指引下打破“单打一”教学实行教师“多面手”",
+        "authors": [],
+        "page_start": 1,
+        "page_end": 4,
+        "dates": []
+      }
+    ]
+  },
+  "parser_id": "automation",
+  "path": "2a5a5a1a-7203-43f2-bf13-2881453e9777",
+  "resource_type": "book",
+  "version": 2
+}
