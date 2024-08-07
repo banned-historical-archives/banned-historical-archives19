e43431599715321a37e594abc883db81e306ef45
Your Name <you@example.com>
2024-08-07 22:48:27 +0800

    1

---
+++ 4ea1dc0e-dab1-4f0e-89bb-a28d2beef9bd.ts
@@ -0,0 +1,28 @@
+export default {
+  "entity": {
+    "id": "4ea1dc0e-dab1-4f0e-89bb-a28d2beef9bd",
+    "name": "清华蒯大富",
+    "internal": false,
+    "type": "pdf",
+    "official": false,
+    "author": "蒯大富",
+    "files": "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives19/main/4ea1dc0e-dab1-4f0e-89bb-a28d2beef9bd.pdf"
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "清华蒯大富",
+        "authors": [
+          "蒯大富"
+        ],
+        "page_start": 1,
+        "page_end": 262,
+        "dates": []
+      }
+    ]
+  },
+  "parser_id": "automation",
+  "path": "4ea1dc0e-dab1-4f0e-89bb-a28d2beef9bd.pdf",
+  "resource_type": "book",
+  "version": 2
+}
