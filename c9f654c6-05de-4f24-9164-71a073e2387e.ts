52f7ccfeed8da8bda574e7fb748fe9147ccbebbc
Your Name <you@example.com>
2024-08-07 21:57:32 +0800

    1

---
+++ c9f654c6-05de-4f24-9164-71a073e2387e.ts
@@ -0,0 +1,28 @@
+export default {
+  "entity": {
+    "id": "c9f654c6-05de-4f24-9164-71a073e2387e",
+    "name": "晚年周恩来",
+    "internal": false,
+    "type": "pdf",
+    "official": false,
+    "author": "高文谦",
+    "files": "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives19/main/c9f654c6-05de-4f24-9164-71a073e2387e.pdf"
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "晚年周恩来",
+        "authors": [
+          "高文谦"
+        ],
+        "page_start": 1,
+        "page_end": 419,
+        "dates": []
+      }
+    ]
+  },
+  "parser_id": "automation",
+  "path": "c9f654c6-05de-4f24-9164-71a073e2387e.pdf",
+  "resource_type": "book",
+  "version": 2
+}
