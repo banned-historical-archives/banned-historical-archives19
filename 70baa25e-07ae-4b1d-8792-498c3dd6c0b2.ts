c23d923018c3a6b40cdb43f802510a3d56244afa
Your Name <you@example.com>
2024-08-07 23:43:33 +0800

    1

---
+++ 70baa25e-07ae-4b1d-8792-498c3dd6c0b2.ts
@@ -0,0 +1,28 @@
+export default {
+  "entity": {
+    "id": "70baa25e-07ae-4b1d-8792-498c3dd6c0b2",
+    "name": "《基层文革泥泞路》（人民文革丛书卷三）",
+    "internal": false,
+    "type": "pdf",
+    "official": false,
+    "author": "刘国凯",
+    "files": "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives19/main/70baa25e-07ae-4b1d-8792-498c3dd6c0b2.pdf"
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "《基层文革泥泞路》（人民文革丛书卷三）",
+        "authors": [
+          "刘国凯"
+        ],
+        "page_start": 1,
+        "page_end": 106,
+        "dates": []
+      }
+    ]
+  },
+  "parser_id": "automation",
+  "path": "70baa25e-07ae-4b1d-8792-498c3dd6c0b2.pdf",
+  "resource_type": "book",
+  "version": 2
+}
