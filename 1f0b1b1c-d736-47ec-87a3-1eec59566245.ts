cd0161f805e3723ec35d3a3caeb1654a94d7c221
Your Name <you@example.com>
2024-08-07 22:27:59 +0800

    1

---
+++ 1f0b1b1c-d736-47ec-87a3-1eec59566245.ts
@@ -0,0 +1,32 @@
+export default {
+  "entity": {
+    "id": "1f0b1b1c-d736-47ec-87a3-1eec59566245",
+    "name": "王力反思录",
+    "internal": false,
+    "type": "pdf",
+    "official": false,
+    "author": "王力",
+    "files": "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives19/main/1f0b1b1c-d736-47ec-87a3-1eec59566245.pdf"
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "王力反思录",
+        "authors": [
+          "王力"
+        ],
+        "page_start": 1,
+        "page_end": 1133,
+        "dates": [
+          {
+            "year": 2006
+          }
+        ]
+      }
+    ]
+  },
+  "parser_id": "automation",
+  "path": "1f0b1b1c-d736-47ec-87a3-1eec59566245.pdf",
+  "resource_type": "book",
+  "version": 2
+}
