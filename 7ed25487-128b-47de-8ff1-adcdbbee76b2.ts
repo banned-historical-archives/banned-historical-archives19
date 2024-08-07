0d4fd8e3688fca7f8769c1e9f090599b3fded71c
Your Name <you@example.com>
2024-08-07 22:18:06 +0800

    1

---
+++ 7ed25487-128b-47de-8ff1-adcdbbee76b2.ts
@@ -0,0 +1,28 @@
+export default {
+  "entity": {
+    "id": "7ed25487-128b-47de-8ff1-adcdbbee76b2",
+    "name": "西安文革狂潮",
+    "internal": false,
+    "type": "pdf",
+    "official": false,
+    "author": "罗玉振",
+    "files": "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives19/main/7ed25487-128b-47de-8ff1-adcdbbee76b2.pdf"
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "西安文革狂潮",
+        "authors": [
+          "罗玉振"
+        ],
+        "page_start": 1,
+        "page_end": 100,
+        "dates": []
+      }
+    ]
+  },
+  "parser_id": "automation",
+  "path": "7ed25487-128b-47de-8ff1-adcdbbee76b2.pdf",
+  "resource_type": "book",
+  "version": 2
+}
