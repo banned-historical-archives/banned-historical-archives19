93dcd1836dd3927706312cf2f3bfcb52549cdf1c
Your Name <you@example.com>
2024-08-08 00:06:19 +0800

    1

---
+++ 471e286e-5893-4903-ab1c-8a55d5fa6327.ts
@@ -0,0 +1,28 @@
+export default {
+  "entity": {
+    "id": "471e286e-5893-4903-ab1c-8a55d5fa6327",
+    "name": "周恩来传（四）",
+    "internal": false,
+    "type": "pdf",
+    "official": false,
+    "author": "金冲及",
+    "files": "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives19/main/471e286e-5893-4903-ab1c-8a55d5fa6327.pdf"
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "周恩来传（四）",
+        "authors": [
+          "金冲及"
+        ],
+        "page_start": 1,
+        "page_end": 823,
+        "dates": []
+      }
+    ]
+  },
+  "parser_id": "automation",
+  "path": "471e286e-5893-4903-ab1c-8a55d5fa6327.pdf",
+  "resource_type": "book",
+  "version": 2
+}
---
+++ a407be55-6558-4ede-8d1a-ad9f3c497b96.ts
@@ -0,0 +1,28 @@
+export default {
+  "entity": {
+    "id": "a407be55-6558-4ede-8d1a-ad9f3c497b96",
+    "name": "周恩来传（二）",
+    "internal": false,
+    "type": "pdf",
+    "official": false,
+    "author": "金冲及",
+    "files": "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives19/main/a407be55-6558-4ede-8d1a-ad9f3c497b96.pdf"
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "周恩来传（二）",
+        "authors": [
+          "金冲及"
+        ],
+        "page_start": 1,
+        "page_end": 672,
+        "dates": []
+      }
+    ]
+  },
+  "parser_id": "automation",
+  "path": "a407be55-6558-4ede-8d1a-ad9f3c497b96.pdf",
+  "resource_type": "book",
+  "version": 2
+}
