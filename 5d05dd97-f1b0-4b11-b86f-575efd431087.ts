73cd584ef5ec46ae47cb060d29ba5df4a15eeceb
Your Name <you@example.com>
2024-08-07 22:45:30 +0800

    1

---
+++ 5d05dd97-f1b0-4b11-b86f-575efd431087.ts
@@ -0,0 +1,34 @@
+export default {
+  "entity": {
+    "id": "5d05dd97-f1b0-4b11-b86f-575efd431087",
+    "name": "光辉的斗争史:湘江风雷挺进纵队总司令部大事记",
+    "internal": false,
+    "type": "pdf",
+    "official": false,
+    "author": "湖南省长沙市高校风雷挺进纵队编",
+    "files": "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives19/main/5d05dd97-f1b0-4b11-b86f-575efd431087.pdf"
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "光辉的斗争史:湘江风雷挺进纵队总司令部大事记",
+        "authors": [
+          "湖南省长沙市高校风雷挺进纵队编"
+        ],
+        "page_start": 1,
+        "page_end": 57,
+        "dates": [
+          {
+            "year": 1967,
+            "month": 7,
+            "day": 7
+          }
+        ]
+      }
+    ]
+  },
+  "parser_id": "automation",
+  "path": "5d05dd97-f1b0-4b11-b86f-575efd431087.pdf",
+  "resource_type": "book",
+  "version": 2
+}
