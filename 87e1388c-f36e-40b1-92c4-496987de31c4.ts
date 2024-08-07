623494b6cb8697ba9340e35ada7f35682f9b7788
Your Name <you@example.com>
2024-08-07 23:42:25 +0800

    1

---
+++ 87e1388c-f36e-40b1-92c4-496987de31c4.ts
@@ -0,0 +1,28 @@
+export default {
+  "entity": {
+    "id": "87e1388c-f36e-40b1-92c4-496987de31c4",
+    "name": "《文化革命简析》（人民文革丛书卷一）",
+    "internal": false,
+    "type": "pdf",
+    "official": false,
+    "author": "刘国凯",
+    "files": "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives19/main/87e1388c-f36e-40b1-92c4-496987de31c4.pdf"
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "《文化革命简析》（人民文革丛书卷一）",
+        "authors": [
+          "刘国凯"
+        ],
+        "page_start": 1,
+        "page_end": 88,
+        "dates": []
+      }
+    ]
+  },
+  "parser_id": "automation",
+  "path": "87e1388c-f36e-40b1-92c4-496987de31c4.pdf",
+  "resource_type": "book",
+  "version": 2
+}
