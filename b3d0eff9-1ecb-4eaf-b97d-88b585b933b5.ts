e9e3688225718cf264cc6b6c0a540909def408b1
Your Name <you@example.com>
2024-08-07 21:51:51 +0800

    1

---
+++ b3d0eff9-1ecb-4eaf-b97d-88b585b933b5.ts
@@ -0,0 +1,32 @@
+export default {
+  "entity": {
+    "id": "b3d0eff9-1ecb-4eaf-b97d-88b585b933b5",
+    "name": "岁月艰难 吴法宪回忆录",
+    "internal": false,
+    "type": "pdf",
+    "official": false,
+    "author": "吴法宪",
+    "files": "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives19/main/b3d0eff9-1ecb-4eaf-b97d-88b585b933b5.pdf"
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "岁月艰难 吴法宪回忆录",
+        "authors": [
+          "吴法宪"
+        ],
+        "page_start": 1,
+        "page_end": 507,
+        "dates": [
+          {
+            "year": 2013
+          }
+        ]
+      }
+    ]
+  },
+  "parser_id": "automation",
+  "path": "b3d0eff9-1ecb-4eaf-b97d-88b585b933b5.pdf",
+  "resource_type": "book",
+  "version": 2
+}
