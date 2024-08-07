6e438a2ada7f6581ac776c761ab0815e9d068cd9
Your Name <you@example.com>
2024-08-07 23:56:57 +0800

    1

---
+++ 564c0ed7-e5ff-4b59-bb1c-60cfe1d1a4bb.ts
@@ -0,0 +1,28 @@
+export default {
+  "entity": {
+    "id": "564c0ed7-e5ff-4b59-bb1c-60cfe1d1a4bb",
+    "name": "爝火不息 文革民间思想研究笔记（上卷）",
+    "internal": false,
+    "type": "pdf",
+    "official": false,
+    "author": "钱理群",
+    "files": "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives19/main/564c0ed7-e5ff-4b59-bb1c-60cfe1d1a4bb.pdf"
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "爝火不息 文革民间思想研究笔记（上卷）",
+        "authors": [
+          "钱理群"
+        ],
+        "page_start": 1,
+        "page_end": 674,
+        "dates": []
+      }
+    ]
+  },
+  "parser_id": "automation",
+  "path": "564c0ed7-e5ff-4b59-bb1c-60cfe1d1a4bb.pdf",
+  "resource_type": "book",
+  "version": 2
+}
