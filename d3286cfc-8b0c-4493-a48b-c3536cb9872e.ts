39524ca9907a5b4398519ca67b6c960fe10c80a9
Your Name <you@example.com>
2024-08-07 22:26:47 +0800

    1

---
+++ d3286cfc-8b0c-4493-a48b-c3536cb9872e.ts
@@ -0,0 +1,32 @@
+export default {
+  "entity": {
+    "id": "d3286cfc-8b0c-4493-a48b-c3536cb9872e",
+    "name": "我的父亲邓小平",
+    "internal": false,
+    "type": "pdf",
+    "official": false,
+    "author": "邓榕",
+    "files": "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives19/main/d3286cfc-8b0c-4493-a48b-c3536cb9872e.pdf"
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "我的父亲邓小平",
+        "authors": [
+          "邓榕"
+        ],
+        "page_start": 1,
+        "page_end": 545,
+        "dates": [
+          {
+            "year": 2000
+          }
+        ]
+      }
+    ]
+  },
+  "parser_id": "automation",
+  "path": "d3286cfc-8b0c-4493-a48b-c3536cb9872e.pdf",
+  "resource_type": "book",
+  "version": 2
+}
