e87b550a408d6290384582625f18c99c7262eb29
Your Name <you@example.com>
2024-08-07 23:46:19 +0800

    1

---
+++ 305b7e91-0cfe-4dbf-aaee-d25ea134098b.ts
@@ -0,0 +1,29 @@
+export default {
+  "entity": {
+    "id": "305b7e91-0cfe-4dbf-aaee-d25ea134098b",
+    "name": "文化大革命和它的异端思潮",
+    "internal": false,
+    "type": "pdf",
+    "official": false,
+    "author": "宋永毅 孙大进",
+    "files": "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives19/main/305b7e91-0cfe-4dbf-aaee-d25ea134098b.pdf"
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "文化大革命和它的异端思潮",
+        "authors": [
+          "宋永毅",
+          "孙大进"
+        ],
+        "page_start": 1,
+        "page_end": 221,
+        "dates": []
+      }
+    ]
+  },
+  "parser_id": "automation",
+  "path": "305b7e91-0cfe-4dbf-aaee-d25ea134098b.pdf",
+  "resource_type": "book",
+  "version": 2
+}
