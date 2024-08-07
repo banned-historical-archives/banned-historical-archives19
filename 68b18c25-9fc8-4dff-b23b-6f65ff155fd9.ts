0fdfdca295ed3c983f90867d7a8a6abff48b5dca
Your Name <you@example.com>
2024-08-07 21:30:14 +0800

    1

---
+++ 68b18c25-9fc8-4dff-b23b-6f65ff155fd9.ts
@@ -0,0 +1,26 @@
+export default {
+  "entity": {
+    "id": "68b18c25-9fc8-4dff-b23b-6f65ff155fd9",
+    "name": "广东文革掠影",
+    "internal": false,
+    "type": "pdf",
+    "official": false,
+    "author": "",
+    "files": "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives19/main/68b18c25-9fc8-4dff-b23b-6f65ff155fd9.pdf"
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "广东文革掠影",
+        "authors": [],
+        "page_start": 1,
+        "page_end": 270,
+        "dates": []
+      }
+    ]
+  },
+  "parser_id": "automation",
+  "path": "68b18c25-9fc8-4dff-b23b-6f65ff155fd9.pdf",
+  "resource_type": "book",
+  "version": 2
+}
