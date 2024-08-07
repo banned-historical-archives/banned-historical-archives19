c5c5d833941f915dd973a6514f884008c0a03b4c
Your Name <you@example.com>
2024-08-07 23:38:06 +0800

    1

---
+++ 0ec08cd4-c406-46c4-9816-1f0d7c246510.ts
@@ -0,0 +1,32 @@
+export default {
+  "entity": {
+    "id": "0ec08cd4-c406-46c4-9816-1f0d7c246510",
+    "name": "失踪者的足迹——文化大革命期间的青年思潮",
+    "internal": false,
+    "type": "pdf",
+    "official": false,
+    "author": "印红标",
+    "files": "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives19/main/0ec08cd4-c406-46c4-9816-1f0d7c246510.pdf"
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "失踪者的足迹——文化大革命期间的青年思潮",
+        "authors": [
+          "印红标"
+        ],
+        "page_start": 1,
+        "page_end": 588,
+        "dates": [
+          {
+            "year": 2009
+          }
+        ]
+      }
+    ]
+  },
+  "parser_id": "automation",
+  "path": "0ec08cd4-c406-46c4-9816-1f0d7c246510.pdf",
+  "resource_type": "book",
+  "version": 2
+}
