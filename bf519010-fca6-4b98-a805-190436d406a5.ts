06b3c6f530045cb7d7afd282dd536e84032ce8ef
Your Name <you@example.com>
2024-08-07 22:37:28 +0800

    1

---
+++ bf519010-fca6-4b98-a805-190436d406a5.ts
@@ -0,0 +1,28 @@
+export default {
+  "entity": {
+    "id": "bf519010-fca6-4b98-a805-190436d406a5",
+    "name": "生逢其时——我亲历的无产阶级文化大革命",
+    "internal": false,
+    "type": "pdf",
+    "official": false,
+    "author": "顾建棠",
+    "files": "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives19/main/bf519010-fca6-4b98-a805-190436d406a5.pdf"
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "生逢其时——我亲历的无产阶级文化大革命",
+        "authors": [
+          "顾建棠"
+        ],
+        "page_start": 1,
+        "page_end": 502,
+        "dates": []
+      }
+    ]
+  },
+  "parser_id": "automation",
+  "path": "bf519010-fca6-4b98-a805-190436d406a5.pdf",
+  "resource_type": "book",
+  "version": 2
+}
