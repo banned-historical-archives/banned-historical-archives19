577936e93d51a93896bc59d73fd0ee28e15e4c5e
Your Name <you@example.com>
2024-08-07 23:47:27 +0800

    1

---
+++ eb9835b6-802f-4603-b1d2-1038bdb1e36e.ts
@@ -0,0 +1,33 @@
+export default {
+  "entity": {
+    "id": "eb9835b6-802f-4603-b1d2-1038bdb1e36e",
+    "name": "文化大革命时期诗歌研究",
+    "internal": false,
+    "type": "pdf",
+    "official": false,
+    "author": "王家平",
+    "files": "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives19/main/eb9835b6-802f-4603-b1d2-1038bdb1e36e.pdf"
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "文化大革命时期诗歌研究",
+        "authors": [
+          "王家平"
+        ],
+        "page_start": 1,
+        "page_end": 330,
+        "dates": [
+          {
+            "year": 2004,
+            "month": 12
+          }
+        ]
+      }
+    ]
+  },
+  "parser_id": "automation",
+  "path": "eb9835b6-802f-4603-b1d2-1038bdb1e36e.pdf",
+  "resource_type": "book",
+  "version": 2
+}
