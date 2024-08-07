e308297235156de713a8f257bac6d4246bb4a09e
Your Name <you@example.com>
2024-08-07 23:49:42 +0800

    1

---
+++ 6629164b-2106-4773-bdac-ee7e4956d000.ts
@@ -0,0 +1,32 @@
+export default {
+  "entity": {
+    "id": "6629164b-2106-4773-bdac-ee7e4956d000",
+    "name": "一场夭折的中国文化启蒙运动",
+    "internal": false,
+    "type": "pdf",
+    "official": false,
+    "author": "林伟然",
+    "files": "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives19/main/6629164b-2106-4773-bdac-ee7e4956d000.pdf"
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "一场夭折的中国文化启蒙运动",
+        "authors": [
+          "林伟然"
+        ],
+        "page_start": 1,
+        "page_end": 216,
+        "dates": [
+          {
+            "year": 1996
+          }
+        ]
+      }
+    ]
+  },
+  "parser_id": "automation",
+  "path": "6629164b-2106-4773-bdac-ee7e4956d000.pdf",
+  "resource_type": "book",
+  "version": 2
+}
