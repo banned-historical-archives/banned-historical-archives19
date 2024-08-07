3dbdc6c97b49d94555182e235b72841e1a8e668c
Your Name <you@example.com>
2024-08-07 21:01:55 +0800

    1

---
+++ 81bf2575-6154-4176-af15-39b8295dcd1f.ts
@@ -0,0 +1,32 @@
+export default {
+  "entity": {
+    "id": "81bf2575-6154-4176-af15-39b8295dcd1f",
+    "name": "陈毅自传",
+    "internal": false,
+    "type": "imgs",
+    "official": false,
+    "author": "",
+    "files": [
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives19/main/81bf2575-6154-4176-af15-39b8295dcd1f/1.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives19/main/81bf2575-6154-4176-af15-39b8295dcd1f/2.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives19/main/81bf2575-6154-4176-af15-39b8295dcd1f/3.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives19/main/81bf2575-6154-4176-af15-39b8295dcd1f/4.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives19/main/81bf2575-6154-4176-af15-39b8295dcd1f/5.jpg"
+    ]
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "陈毅自传",
+        "authors": [],
+        "page_start": 1,
+        "page_end": 5,
+        "dates": []
+      }
+    ]
+  },
+  "parser_id": "automation",
+  "path": "81bf2575-6154-4176-af15-39b8295dcd1f",
+  "resource_type": "book",
+  "version": 2
+}
