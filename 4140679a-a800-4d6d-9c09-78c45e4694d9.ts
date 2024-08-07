c6c439dadc0b412ab1386d8ec66cca31ea3f624e
Your Name <you@example.com>
2024-08-07 23:54:44 +0800

    1

---
+++ 4140679a-a800-4d6d-9c09-78c45e4694d9.ts
@@ -0,0 +1,28 @@
+export default {
+  "entity": {
+    "id": "4140679a-a800-4d6d-9c09-78c45e4694d9",
+    "name": "既非一个文革，也非两个文革",
+    "internal": false,
+    "type": "pdf",
+    "official": false,
+    "author": "乔晞华",
+    "files": "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives19/main/4140679a-a800-4d6d-9c09-78c45e4694d9.pdf"
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "既非一个文革，也非两个文革",
+        "authors": [
+          "乔晞华"
+        ],
+        "page_start": 1,
+        "page_end": 177,
+        "dates": []
+      }
+    ]
+  },
+  "parser_id": "automation",
+  "path": "4140679a-a800-4d6d-9c09-78c45e4694d9.pdf",
+  "resource_type": "book",
+  "version": 2
+}
