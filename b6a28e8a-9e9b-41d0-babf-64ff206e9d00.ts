959f2487fedfc1fa9c686e6953363f022d46a309
Your Name <you@example.com>
2024-08-07 21:53:51 +0800

    1

---
+++ b6a28e8a-9e9b-41d0-babf-64ff206e9d00.ts
@@ -0,0 +1,28 @@
+export default {
+  "entity": {
+    "id": "b6a28e8a-9e9b-41d0-babf-64ff206e9d00",
+    "name": "李作鹏回忆录",
+    "internal": false,
+    "type": "pdf",
+    "official": false,
+    "author": "李作鹏",
+    "files": "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives19/main/b6a28e8a-9e9b-41d0-babf-64ff206e9d00.pdf"
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "李作鹏回忆录",
+        "authors": [
+          "李作鹏"
+        ],
+        "page_start": 1,
+        "page_end": 1017,
+        "dates": []
+      }
+    ]
+  },
+  "parser_id": "automation",
+  "path": "b6a28e8a-9e9b-41d0-babf-64ff206e9d00.pdf",
+  "resource_type": "book",
+  "version": 2
+}
