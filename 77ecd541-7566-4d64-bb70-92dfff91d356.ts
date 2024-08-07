d1987cc543d7875f69be77f3c058a7a41cd58dbc
Your Name <you@example.com>
2024-08-07 21:12:58 +0800

    1

---
+++ 77ecd541-7566-4d64-bb70-92dfff91d356.ts
@@ -0,0 +1,33 @@
+export default {
+  "entity": {
+    "id": "77ecd541-7566-4d64-bb70-92dfff91d356",
+    "name": "人民公社研究",
+    "internal": false,
+    "type": "pdf",
+    "official": false,
+    "author": "张乐天",
+    "files": "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives19/main/77ecd541-7566-4d64-bb70-92dfff91d356.pdf"
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "人民公社研究",
+        "authors": [
+          "张乐天"
+        ],
+        "page_start": 1,
+        "page_end": 467,
+        "dates": [
+          {
+            "year": 2005,
+            "month": 12
+          }
+        ]
+      }
+    ]
+  },
+  "parser_id": "automation",
+  "path": "77ecd541-7566-4d64-bb70-92dfff91d356.pdf",
+  "resource_type": "book",
+  "version": 2
+}
