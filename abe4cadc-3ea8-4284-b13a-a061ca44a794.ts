7e572ad6ae453addd06555486258d0d3a14f5756
Your Name <you@example.com>
2024-08-07 22:31:05 +0800

    1

---
+++ abe4cadc-3ea8-4284-b13a-a061ca44a794.ts
@@ -0,0 +1,28 @@
+export default {
+  "entity": {
+    "id": "abe4cadc-3ea8-4284-b13a-a061ca44a794",
+    "name": "从儿童团员到造反派-高飞回忆录",
+    "internal": false,
+    "type": "pdf",
+    "official": false,
+    "author": "高飞",
+    "files": "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives19/main/abe4cadc-3ea8-4284-b13a-a061ca44a794.pdf"
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "从儿童团员到造反派-高飞回忆录",
+        "authors": [
+          "高飞"
+        ],
+        "page_start": 1,
+        "page_end": 498,
+        "dates": []
+      }
+    ]
+  },
+  "parser_id": "automation",
+  "path": "abe4cadc-3ea8-4284-b13a-a061ca44a794.pdf",
+  "resource_type": "book",
+  "version": 2
+}
