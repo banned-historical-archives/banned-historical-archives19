fec5eed901f2dd27c59e29a525a422eed360a8df
Your Name <you@example.com>
2024-08-07 23:52:18 +0800

    1

---
+++ cc8d1234-ca2c-44ed-af31-d81482f6760a.ts
@@ -0,0 +1,28 @@
+export default {
+  "entity": {
+    "id": "cc8d1234-ca2c-44ed-af31-d81482f6760a",
+    "name": "文革群众运动的动员、分裂与灭亡——以社会运动学视角",
+    "internal": false,
+    "type": "pdf",
+    "official": false,
+    "author": "乔晞华",
+    "files": "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives19/main/cc8d1234-ca2c-44ed-af31-d81482f6760a.pdf"
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "文革群众运动的动员、分裂与灭亡——以社会运动学视角",
+        "authors": [
+          "乔晞华"
+        ],
+        "page_start": 1,
+        "page_end": 356,
+        "dates": []
+      }
+    ]
+  },
+  "parser_id": "automation",
+  "path": "cc8d1234-ca2c-44ed-af31-d81482f6760a.pdf",
+  "resource_type": "book",
+  "version": 2
+}
