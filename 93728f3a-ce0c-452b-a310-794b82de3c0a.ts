8f97ec7aa29e963c0a44ec9df97e039617f2aee5
Your Name <you@example.com>
2024-08-07 22:25:14 +0800

    1

---
+++ 93728f3a-ce0c-452b-a310-794b82de3c0a.ts
@@ -0,0 +1,32 @@
+export default {
+  "entity": {
+    "id": "93728f3a-ce0c-452b-a310-794b82de3c0a",
+    "name": "十年一梦",
+    "internal": false,
+    "type": "pdf",
+    "official": false,
+    "author": "徐景贤",
+    "files": "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives19/main/93728f3a-ce0c-452b-a310-794b82de3c0a.pdf"
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "十年一梦",
+        "authors": [
+          "徐景贤"
+        ],
+        "page_start": 1,
+        "page_end": 450,
+        "dates": [
+          {
+            "year": 2005
+          }
+        ]
+      }
+    ]
+  },
+  "parser_id": "automation",
+  "path": "93728f3a-ce0c-452b-a310-794b82de3c0a.pdf",
+  "resource_type": "book",
+  "version": 2
+}
