822e51bcd9c46baac102459579a8eb2ff93255b7
Your Name <you@example.com>
2024-08-07 22:46:12 +0800

    1

---
+++ 66d34f77-67bd-477a-bfb2-ccd8f470e4f9.ts
@@ -0,0 +1,32 @@
+export default {
+  "entity": {
+    "id": "66d34f77-67bd-477a-bfb2-ccd8f470e4f9",
+    "name": "青春无痕：一个造反派工人的十年文革",
+    "internal": false,
+    "type": "pdf",
+    "official": false,
+    "author": "陈益南",
+    "files": "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives19/main/66d34f77-67bd-477a-bfb2-ccd8f470e4f9.pdf"
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "青春无痕：一个造反派工人的十年文革",
+        "authors": [
+          "陈益南"
+        ],
+        "page_start": 1,
+        "page_end": 371,
+        "dates": [
+          {
+            "year": 2006
+          }
+        ]
+      }
+    ]
+  },
+  "parser_id": "automation",
+  "path": "66d34f77-67bd-477a-bfb2-ccd8f470e4f9.pdf",
+  "resource_type": "book",
+  "version": 2
+}
