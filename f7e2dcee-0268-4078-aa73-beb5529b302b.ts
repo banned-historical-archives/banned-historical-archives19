ea547757fdfbdb014e18490f8f283ed76e6ab1b1
Your Name <you@example.com>
2024-08-07 23:02:04 +0800

    1

---
+++ f7e2dcee-0268-4078-aa73-beb5529b302b.ts
@@ -0,0 +1,33 @@
+export default {
+  "entity": {
+    "id": "f7e2dcee-0268-4078-aa73-beb5529b302b",
+    "name": "三线风云：中国三线建设文选第2集",
+    "internal": false,
+    "type": "pdf",
+    "official": false,
+    "author": "倪同正",
+    "files": "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives19/main/f7e2dcee-0268-4078-aa73-beb5529b302b.pdf"
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "三线风云：中国三线建设文选第2集",
+        "authors": [
+          "倪同正"
+        ],
+        "page_start": 1,
+        "page_end": 514,
+        "dates": [
+          {
+            "year": 2013,
+            "month": 4
+          }
+        ]
+      }
+    ]
+  },
+  "parser_id": "automation",
+  "path": "f7e2dcee-0268-4078-aa73-beb5529b302b.pdf",
+  "resource_type": "book",
+  "version": 2
+}
