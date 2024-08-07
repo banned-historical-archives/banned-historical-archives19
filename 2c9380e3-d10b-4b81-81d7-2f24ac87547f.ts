4eb2b8cd79986cb912b03beccf4f9952dc590be0
Your Name <you@example.com>
2024-08-07 22:49:40 +0800

    1

---
+++ 2c9380e3-d10b-4b81-81d7-2f24ac87547f.ts
@@ -0,0 +1,32 @@
+export default {
+  "entity": {
+    "id": "2c9380e3-d10b-4b81-81d7-2f24ac87547f",
+    "name": "王大宾回忆录",
+    "internal": false,
+    "type": "pdf",
+    "official": false,
+    "author": "王大宾",
+    "files": "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives19/main/2c9380e3-d10b-4b81-81d7-2f24ac87547f.pdf"
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "王大宾回忆录",
+        "authors": [
+          "王大宾"
+        ],
+        "page_start": 1,
+        "page_end": 276,
+        "dates": [
+          {
+            "year": 2015
+          }
+        ]
+      }
+    ]
+  },
+  "parser_id": "automation",
+  "path": "2c9380e3-d10b-4b81-81d7-2f24ac87547f.pdf",
+  "resource_type": "book",
+  "version": 2
+}
