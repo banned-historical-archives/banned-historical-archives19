3fe316492071940387190056089cb5ffb8bb35a5
Your Name <you@example.com>
2024-08-08 00:04:50 +0800

    1

---
+++ 27bc6fb0-381a-4879-b112-16781c07558a.ts
@@ -0,0 +1,28 @@
+export default {
+  "entity": {
+    "id": "27bc6fb0-381a-4879-b112-16781c07558a",
+    "name": "周恩来传（三）",
+    "internal": false,
+    "type": "pdf",
+    "official": false,
+    "author": "金冲及",
+    "files": "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives19/main/27bc6fb0-381a-4879-b112-16781c07558a.pdf"
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "周恩来传（三）",
+        "authors": [
+          "金冲及"
+        ],
+        "page_start": 1,
+        "page_end": 793,
+        "dates": []
+      }
+    ]
+  },
+  "parser_id": "automation",
+  "path": "27bc6fb0-381a-4879-b112-16781c07558a.pdf",
+  "resource_type": "book",
+  "version": 2
+}
